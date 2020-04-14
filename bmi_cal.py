d2faa9ec7dc0db42f75fc3faae2785f74e04b69a
Shannon Jeet Singh <singhshannon95@gmail.com>
2020-04-14 13:32:59 +0530

    BMI Calculator

---
+++ bmi_cal.py
@@ -0,0 +1,19 @@
+#Assignment 1
+"""
+This is a program on Adult Body Mass Index Calculator
+
+Submitted by:Shannon Jeet Singh
+Partipitant ID:5904
+Submitted to:Forsk Coding School, Jaipur
+
+"""
+
+
+a=float(input("Enter your Height(in meters)"))
+b=float(input("Enter your Weight(in kg)"))
+c=(b/a)
+d=(c/a)
+print("Your BMI is", d)
+
+      
+      
